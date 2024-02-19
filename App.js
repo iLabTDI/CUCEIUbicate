1fd302d09bc779e6842b3c277f9d00d0ec62affe
yairhdz24 <yairhdz107@gmail.com>
2024-02-19 12:59:55 -0600

    cambio de home page a MyDrawer como main

--- App.js
+++ App.js
@@ -1,56 +1,54 @@
-import React, { useState } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-// import { HomePage } from './Pages/HomePage';
-// import { LoginScreen } from './auth/LoginScreen'; 
-// import { CompleteProfile } from './auth/CompleteProfile';
-// import { RegisterScreen } from './auth/RegisterScreen';
-// import { View } from 'react-native-animatable';
-import { MyDrawer } from './Components/NavBar';
+import React, { useState } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+// import { HomePage } from "./Pages/HomePage";
+import { LoginScreen } from "./auth/LoginScreen";
+import { CompleteProfile } from "./auth/CompleteProfile";
+import { RegisterScreen } from "./auth/RegisterScreen";
+import { View } from "react-native-animatable";
+import { MyDrawer } from "./Components/NavBar";
 
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
-
     //para editar cada pantalla
 
-    <NavigationContainer>
-     <MyDrawer /> 
-   </NavigationContainer> 
+    //   <NavigationContainer>
+    //    <MyDrawer />
+    //  </NavigationContainer>
 
-    // <NavigationContainer>
-    //   <Stack.Navigator initialRouteName="Inicio">
-    //     <Stack.Screen
-    //       name="Inicio"
-    //       component={LoginScreen}
-    //       options={{ title: 'Inicio', headerShown: false }} // Oculta la barra de navegación
-    //     />
-    //     <Stack.Screen
-    //       name="Login"
-    //       component={LoginScreen}
-    //       options={{ title: 'Iniciar Sesión' }}
-    //     />
-    //     <Stack.Screen
-    //       name="Registro"
-    //       component={RegisterScreen}
-    //       options={{ title: 'Registro' }}
-    //     />
-    //     <Stack.Screen
-    //       name="Completar Perfil"
-    //       component={CompleteProfile}
-    //       options={{ title: 'Completar Perfil' }}
-    //     />
-    //     <Stack.Screen
-    //       name="Principal Home"
-    //       component={HomePage}
-    //       options={{
-    //         headerShown: false 
-    //         }
-    //       }
-    //     />
-    //   </Stack.Navigator>
-    // </NavigationContainer>
-   );
- }
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Inicio">
+        <Stack.Screen
+          name="Inicio"
+          component={LoginScreen}
+          options={{ title: "Inicio", headerShown: false }} // Oculta la barra de navegación
+        />
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ title: "Iniciar Sesión" }}
+        />
+        <Stack.Screen
+          name="Registro"
+          component={RegisterScreen}
+          options={{ title: "Registro" }}
+        />
+        <Stack.Screen
+          name="Completar Perfil"
+          component={CompleteProfile}
+          options={{ title: "Completar Perfil" }}
+        />
+        <Stack.Screen
+          name="Principal Home"
+          component={MyDrawer}
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 export default App;
