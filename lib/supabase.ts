a39ff8a067001a67e9bcace687710b557bf7e542
yairhdz24 <yairhdz107@gmail.com>
2024-04-16 14:17:50 -0600

    credenciales de la bd (cambiar)

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -18,10 +18,10 @@ const ExpoSecureStoreAdapter = {
 }
 
 //modifica estas constantes apra conectar tu base de pruebas
-const supabaseUrl = '';  //https://zyzkfthsghyynigudpsk.supabase.co
+const supabaseUrl = 'https://zyzkfthsghyynigudpsk.supabase.co';  //https://zyzkfthsghyynigudpsk.supabase.co (MAIN)https://pkxynuqxusbwhgjvyssq.supabase.co
 //esta constante tambien
-const supabaseAnonKey = ''; 
-
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp5emtmdGhzZ2h5eW5pZ3VkcHNrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDk1NzgxMzUsImV4cCI6MjAyNTE1NDEzNX0.K4_5toTOm_fgsds95GNeIgWEXyMTmtiM5I7x6bdwe-A'; 
+//MAIN eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBreHludXF4dXNid2hnanZ5c3NxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTAyNTkwMTgsImV4cCI6MjAyNTgzNTAxOH0.uBv5-70jUw0vMLk-PZ1xBOlZbhaUPLeauLLl15jUocQ
 //eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp5emtmdGhzZ2h5eW5pZ3VkcHNrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDk1NzgxMzUsImV4cCI6MjAyNTE1NDEzNX0.K4_5toTOm_fgsds95GNeIgWEXyMTmtiM5I7x6bdwe-A
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
