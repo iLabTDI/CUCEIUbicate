fe34cd83bc338befe73c89b802b13def24619473
yairhdz24 <yairhdz107@gmail.com>
2024-07-19 17:30:11 -0600

    ```text feat: Add School_services screen component

---
+++ Src/Screens/Services/School_services.js
@@ -0,0 +1,20 @@
+import * as React from "react";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+
+
+
+export const School_services = () =>{
+    return(
+        <View style={styles.container}>
+            <Text>School_services</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+});
