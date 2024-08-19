438a16d64310c0c6b09c30fbcd21494139c115aa
yairhdz24 <yairhdz107@gmail.com>
2024-08-18 23:19:03 -0600

    Se modificaron props, tanto la busqueda especifica de los edificios

--- Src/Screens/Home/HomePage.js
+++ Src/Screens/Home/HomePage.js
@@ -1,25 +1,15 @@
-import React, { useState, useRef } from "react";
-import {
-  View,
-  StyleSheet,
-  TouchableOpacity,
-  Image,
-  Dimensions,
-} from "react-native";
-import { useNavigation } from "@react-navigation/native";
-import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
-import { faBars, faRoute, faUser } from "@fortawesome/free-solid-svg-icons";
-import {
-  PinchGestureHandler,
-  GestureHandlerRootView,
-} from "react-native-gesture-handler";
-import ImageZoom from "react-native-image-pan-zoom";
-import { SearchRoute } from "./Components/Layout/SearchRoute";
-import { SpecificSearch } from "./Components/Layout/SearchSpecific";
-import { BottomSheetComponent } from "./Components/BottonSheetComponent/BottonSheet";
-import { MapPoints } from "./Components/MapComponent/MapPoints";
-import RouteDrawer from "./Components/MapComponent/RouteDrawer";
-import { points } from "./Components/MapComponent/data";
+import React, { useState, useRef } from 'react';
+import { View, StyleSheet, TouchableOpacity, Image, Dimensions } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faBars, faRoute, faUser } from '@fortawesome/free-solid-svg-icons';
+import { PinchGestureHandler, GestureHandlerRootView } from 'react-native-gesture-handler';
+import ImageZoom from 'react-native-image-pan-zoom';
+import { SearchRoute } from './Components/Layout/SearchRoute';
+import { SpecificSearch } from './Components/Layout/SearchSpecific';
+import { BottomSheetComponent } from './Components/BottonSheetComponent/BottonSheet';
+import { MapWithPointsAndRoutes } from './Components/MapComponent/MapPoints'; // Asegúrate de que la ruta sea correcta
+import { points } from './Components/MapComponent/data';
 
 export const HomePage = () => {
   const navigation = useNavigation();
@@ -29,42 +19,13 @@ export const HomePage = () => {
   const [selectedRoute, setSelectedRoute] = useState(null);
   const [showSpecificSearch, setShowSpecificSearch] = useState(false);
 
-  // const points = [
-  //   {
-  //     id: "point1",
-  //     name: "Punto 1",
-  //     left: 200,
-  //     top: 300,
-  //     width: 90,
-  //     height: 20,
-  //   },
-  //   {
-  //     id: "Modulo X",
-  //     name: "Módulo X",
-  //     left: 265,
-  //     top: 488,
-  //     width: 90,
-  //     height: 20,
-  //   },
-  //   {
-  //     id: "Modulo Z",
-  //     name: "Módulo Z",
-  //     left: 187,
-  //     top: 649,
-  //     height: 45,
-  //     width: 18,
-  //     transform: [{ rotate: "5deg" }],
-  //   },
-  //   // Agrega más puntos aquí
-  // ];
-
   const handlePointPress = (pointId) => {
     setSelectedPoint(pointId);
     bottomSheetRef.current?.expand();
   };
 
   const handleZoomEvent = (event) => {
-    console.log("Zoom Scale:", event.nativeEvent.scale);
+    console.log('Zoom Scale:', event.nativeEvent.scale);
   };
 
   const toggleSearchBar = () => {
@@ -81,16 +42,21 @@ export const HomePage = () => {
 
   const handleSpecificSearch = (pointId) => {
     setSelectedPoint(pointId);
+    bottomSheetRef.current?.expand(); // Abrir BottomSheet si se selecciona un punto
     setShowSpecificSearch(false);
   };
 
   const handleProfilePress = () => {
-    navigation.navigate("Perfil");
+    navigation.navigate('Perfil');
   };
 
   const handleSearch = (search) => {
     setSelectedRoute(search);
     setShowSearchBar(false);
+  };
+
+  const clearRoute = () => {
+    setSelectedRoute(null);
   };
 
   return (
@@ -119,8 +85,8 @@ export const HomePage = () => {
       )}
 
       <SpecificSearch
-        onSearch={handleSpecificSearch}
         points={points}
+        onSearch={handleSpecificSearch}
         setShowSpecificSearch={setShowSpecificSearch}
       />
 
@@ -129,8 +95,8 @@ export const HomePage = () => {
           onGestureEvent={handleZoomEvent}
           onHandlerStateChange={handleZoomEvent}>
           <ImageZoom
-            cropWidth={Dimensions.get("window").width}
-            cropHeight={Dimensions.get("window").height}
+            cropWidth={Dimensions.get('window').width}
+            cropHeight={Dimensions.get('window').height}
             imageWidth={1600}
             imageHeight={1400}
             enableSwipeDown={false}
@@ -141,23 +107,23 @@ export const HomePage = () => {
             centerOn={{ x: 250, y: -90, scale: 1.1 }}
             maxOverflow={0}>
             <Image
-              source={require("./assets/images/mapa2.webp")}
+              source={require('./assets/images/mapa2.webp')}
               style={styles.image}
             />
-            <MapPoints
+            <MapWithPointsAndRoutes
               onPointPress={handlePointPress}
               selectedRoute={selectedRoute}
               selectedPoint={selectedPoint}
               points={points}
+              clearRoute={clearRoute}
             />
-            <RouteDrawer route={selectedRoute} points={points} />
           </ImageZoom>
         </PinchGestureHandler>
       </GestureHandlerRootView>
 
       <BottomSheetComponent
         ref={bottomSheetRef}
-        snapPoints={["1%", "35%"]}
+        snapPoints={['1%', '35%']}
         selectedPoint={selectedPoint}
         onClose={handleCloseBottomSheet}
       />
@@ -170,37 +136,37 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   overlay: {
-    position: "absolute",
+    position: 'absolute',
     top: 0,
     left: 0,
     right: 0,
     bottom: 0,
-    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
     zIndex: 1,
   },
   menu_icon: {
-    position: "absolute",
+    position: 'absolute',
     top: 45,
     left: 10,
-    backgroundColor: "blue",
+    backgroundColor: 'blue',
     borderRadius: 35,
     padding: 15,
     zIndex: 2,
   },
   search_icon: {
-    position: "absolute",
+    position: 'absolute',
     top: 45,
     right: 70,
-    backgroundColor: "blue",
+    backgroundColor: 'blue',
     borderRadius: 25,
     padding: 15,
     zIndex: 2,
   },
   profile_icon: {
-    position: "absolute",
+    position: 'absolute',
     top: 45,
     right: 305,
-    backgroundColor: "blue",
+    backgroundColor: 'blue',
     borderRadius: 25,
     padding: 15,
     zIndex: 2,
@@ -212,7 +178,7 @@ const styles = StyleSheet.create({
     flex: 1,
     width: undefined,
     height: undefined,
-    alignSelf: "stretch",
+    alignSelf: 'stretch',
   },
 });
 
