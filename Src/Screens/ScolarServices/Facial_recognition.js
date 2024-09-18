697349c4e350b4c6ffa1704258c41040a224afb6
yairhdz24 <yairhdz107@gmail.com>
2024-09-18 17:22:18 -0600

    Refactor: UI del recnocimiento facial

--- Src/Screens/ScolarServices/Facial_recognition.js
+++ Src/Screens/ScolarServices/Facial_recognition.js
@@ -1,45 +1,52 @@
-import * as React from "react";
-import { View, Text, TouchableOpacity, StyleSheet, ScrollView, Linking } from "react-native";
-import jsonData from "../../../assets/jsons/face_access.json";
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+} from 'react-native';
+import jsonData from '../../../assets/jsons/face_access.json';
 
 export const Facial_recognition = () => {
   return (
-    <View style={styles.container}>
-        <View style={styles.card}>
-            <Text style={styles.title}>{jsonData["section-description"].name}</Text>
-            <Text style={styles.text}>{jsonData["section-description"].description}</Text>
-        </View>
-    </View>
+    <View style={styles.content}>
+      <View style={styles.card}>
+        <Text style={styles.title}>{jsonData["section-description"].name}</Text>
+        <Text style={styles.descriptionText}>
+          {jsonData["section-description"].description}
+        </Text>
+      </View>
+      </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    backgroundColor: '#f5f5f5',
+  },
+  content: {
     padding: 20,
-    alignItems: "center",
   },
   card: {
     backgroundColor: '#fff',
-    padding: 16,
-    marginVertical: 8,
-    borderRadius: 8,
+    padding: 20,
+    marginBottom: 20,
+    borderRadius: 10,
     shadowColor: '#000',
     shadowOpacity: 0.1,
-    shadowRadius: 4,
-    shadowOffset: { width: 0, height: 2 },
-    elevation: 3,
+    shadowRadius: 10,
+    shadowOffset: { width: 0, height: 5 },
+    elevation: 5,
   },
   title: {
-    fontSize: 18,
-    fontWeight: "bold",
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#0b34b0',
     marginBottom: 10,
   },
-  text: {
+  descriptionText: {
     fontSize: 16,
-    marginBottom: 5,
-  },
-  link: {
-    color: "blue",
-    marginTop: 10,
+    color: '#333',
+    lineHeight: 24,
   },
 });
