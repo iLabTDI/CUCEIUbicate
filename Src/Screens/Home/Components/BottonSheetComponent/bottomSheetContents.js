2a2c76ed2dc4631d257b6f2b2109e0395ce38fe1
yairhdz24 <yairhdz107@gmail.com>
2024-09-20 21:58:16 -0600

    add: Nuevo estilo, cambio en la data, agregue mas informacion para cada edificio

---
+++ Src/Screens/Home/Components/BottonSheetComponent/bottomSheetContents.js
@@ -0,0 +1,30 @@
+export const bottomSheetContents = [
+    {
+      id: "Modulo X",
+      imageSource: require("./assets/Modulos/Modulo_X.jpeg"),
+      description: "El Módulo X es un edificio moderno y funcional diseñado para albergar aulas y laboratorios de última generación.",
+      rating: 4.5,
+      comments: [
+        { user: "Yair", text: "Excelentes instalaciones, muy cómodo para estudiar." },
+        { user: "Hernandez", text: "El aire acondicionado funciona perfectamente, ideal para los días calurosos." }
+      ],
+      recommendations: [
+        "Visita el laboratorio de informática en el segundo piso.",
+        "No te pierdas la vista desde la terraza en el último piso."
+      ]
+    },
+    {
+      id: "Modulo Z",
+      imageSource: require("./assets/Modulos/Modulo_X.jpeg"),
+      description: "El Módulo Z es conocido por su arquitectura innovadora y sus espacios versátiles para diferentes actividades académicas.",
+      rating: 4.2,
+      comments: [
+        { user: "Yair", text: "Me encanta la biblioteca de este módulo, siempre encuentro un lugar tranquilo para estudiar." },
+        { user: "Hernandez", text: "Los salones son espaciosos y bien iluminados." }
+      ],
+      recommendations: [
+        "Aprovecha las salas de estudio grupal en el tercer piso.",
+        "Visita la cafetería en la planta baja, tienen excelente café."
+      ]
+    },
+  ];
