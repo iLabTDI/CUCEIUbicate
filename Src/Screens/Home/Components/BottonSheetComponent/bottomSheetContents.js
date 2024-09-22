5f0bf529f3aa6cc24c2543ce5713c85150897356
yairhdz24 <yairhdz107@gmail.com>
2024-09-22 02:46:02 -0600

    converti la simagenes en arrays para poner mas de una en un carrusel

--- Src/Screens/Home/Components/BottonSheetComponent/bottomSheetContents.js
+++ Src/Screens/Home/Components/BottonSheetComponent/bottomSheetContents.js
@@ -1,30 +1,90 @@
 export const bottomSheetContents = [
-    {
-      id: "Modulo X",
-      imageSource: require("./assets/Modulos/Modulo_X.jpeg"),
-      description: "El Módulo X es un edificio moderno y funcional diseñado para albergar aulas y laboratorios de última generación.",
-      rating: 4.5,
-      comments: [
-        { user: "Yair", text: "Excelentes instalaciones, muy cómodo para estudiar." },
-        { user: "Hernandez", text: "El aire acondicionado funciona perfectamente, ideal para los días calurosos." }
-      ],
-      recommendations: [
-        "Visita el laboratorio de informática en el segundo piso.",
-        "No te pierdas la vista desde la terraza en el último piso."
-      ]
-    },
-    {
-      id: "Modulo Z",
-      imageSource: require("./assets/Modulos/Modulo_X.jpeg"),
-      description: "El Módulo Z es conocido por su arquitectura innovadora y sus espacios versátiles para diferentes actividades académicas.",
-      rating: 4.2,
-      comments: [
-        { user: "Yair", text: "Me encanta la biblioteca de este módulo, siempre encuentro un lugar tranquilo para estudiar." },
-        { user: "Hernandez", text: "Los salones son espaciosos y bien iluminados." }
-      ],
-      recommendations: [
-        "Aprovecha las salas de estudio grupal en el tercer piso.",
-        "Visita la cafetería en la planta baja, tienen excelente café."
-      ]
-    },
-  ];
+  {
+    id: "Modulo X",
+    images: [require("./assets/Modulo_X.jpeg")],
+    description:
+      "El Módulo X es un edificio moderno y funcional diseñado para albergar aulas y laboratorios de última generación.",
+    rating: 4.5,
+    comments: [
+      {
+        user: "Yair",
+        text: "Excelentes instalaciones, muy cómodo para estudiar.",
+      },
+      {
+        user: "Hernandez",
+        text: "El aire acondicionado funciona perfectamente, ideal para los días calurosos.",
+      },
+    ],
+    recommendations: [
+      "Visita el laboratorio de informática en el segundo piso.",
+      "No te pierdas la vista desde la terraza en el último piso.",
+    ],
+  },
+  {
+    id: "Modulo Z",
+    images: [require("./assets/Modulos/Modulo_Z/Z.png")],
+    description:
+      "El Módulo Z es conocido por su arquitectura innovadora y sus espacios versátiles para diferentes actividades académicas.",
+    rating: 4.2,
+    comments: [
+      {
+        user: "Yair",
+        text: "Me encanta la biblioteca de este módulo, siempre encuentro un lugar tranquilo para estudiar.",
+      },
+      {
+        user: "Hernandez",
+        text: "Los salones son espaciosos y bien iluminados.",
+      },
+    ],
+    recommendations: [
+      "Aprovecha las salas de estudio grupal en el tercer piso.",
+      "Visita la cafetería en la planta baja, tienen excelente café.",
+    ],
+  },
+  {
+    id: "Modulo W",
+    images: [
+      require("./assets/Modulos/Modulo_W/W PRIMER PISO.png"),
+      require("./assets/Modulos/Modulo_W/W SEGUNDO PISO.png"),
+      require("./assets/Modulos/Modulo_W/W TERCER PISO.png"),
+    ],
+    description:
+      "El Módulo W es conocido por su arquitectura innovadora y sus espacios versátiles para diferentes actividades académicas.",
+    rating: 4.2,
+    comments: [
+      {
+        user: "Yair",
+        text: "Me encanta la biblioteca de este módulo, siempre encuentro un lugar tranquilo para estudiar.",
+      },
+      {
+        user: "Hernandez",
+        text: "Los salones son espaciosos y bien iluminados.",
+      },
+    ],
+    recommendations: [
+      "Aprovecha las salas de estudio grupal en el tercer piso.",
+      "Visita la cafetería en la planta baja, tienen excelente café.",
+    ],
+  },
+  {
+    id: "Modulo V2",
+    images: [require("./assets/Modulos/Modulo_V2/V2.png")],
+    description:
+      "El Módulo V2 es conocido por su arquitectura innovadora y sus espacios versátiles para diferentes actividades académicas.",
+    rating: 4.2,
+    comments: [
+      {
+        user: "Yair",
+        text: "Me encanta la biblioteca de este módulo, siempre encuentro un lugar tranquilo para estudiar.",
+      },
+      {
+        user: "Hernandez",
+        text: "Los salones son espaciosos y bien iluminados.",
+      },
+    ],
+    recommendations: [
+      "Aprovecha las salas de estudio grupal en el tercer piso.",
+      "Visita la cafetería en la planta baja, tienen excelente café.",
+    ],
+  },
+];
