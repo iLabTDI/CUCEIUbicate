3965d7db9795f58fe933d2d3a26b0ed1373c017a
yairhdz24 <yairhdz107@gmail.com>
2024-09-26 01:21:50 -0600

    Se asiganron todas las imagenes con su id correspondiente en el content de los bottonshets

--- Src/Screens/Home/Components/BottonSheetComponent/bottomSheetContents.js
+++ Src/Screens/Home/Components/BottonSheetComponent/bottomSheetContents.js
@@ -1,20 +1,285 @@
 export const bottomSheetContents = [
   {
-    id: "Modulo X",
-    images: [require("./assets/Modulo_X.jpeg")],
+    id: "Modulo A",
+    images: [require("./Modulos/Modulo_A/PRIMER_PISO.png")],
+    images: [require("./Modulos/Modulo_A/SEGUNDO_PISO.png")],
     description:
-      "El Módulo X es un edificio moderno y funcional diseñado para albergar aulas y laboratorios de última generación.",
-    rating: 4.5,
-    comments: [
-      {
-        user: "Yair",
-        text: "Excelentes instalaciones, muy cómodo para estudiar.",
-      },
-      {
-        user: "Hernandez",
-        text: "El aire acondicionado funciona perfectamente, ideal para los días calurosos.",
-      },
+      "El Módulo A alberga aulas amplias y es ideal para conferencias y seminarios.",
+    recommendations: [
+      "Visita el auditorio en el segundo piso.",
+      "Explora la sala de estudios en el primer piso.",
     ],
+  },
+  // {
+  //   id: "Modulo B",
+  //   images: [require("./Modulos/Modulo_B/B_CUCS.png")],
+  //   description:
+  //     "El Módulo B se destaca por su diseño clásico y su funcionalidad para actividades administrativas.",
+  //   recommendations: [
+  //     "Revisa los espacios administrativos en el primer piso.",
+  //     "Encuentra la sala de profesores en el segundo piso.",
+  //   ],
+  // },
+  // {
+  //   id: "Modulo C",
+  //   images: [require("./Modulos/Modulo_C/C.png")],
+  //   description:
+  //     "El Módulo C es conocido por albergar laboratorios de ciencia y tecnología.",
+  //   recommendations: [
+  //     "Visita los laboratorios de biotecnología en el primer piso.",
+  //     "Explora el área de investigación en el tercer piso.",
+  //   ],
+  // },
+  {
+    id: "Modulo D",
+    images: [require("./Modulos/Modulo_D/D.png")],
+    description:
+      "El Módulo D ofrece espacios modernos para actividades académicas y administrativas.",
+    recommendations: [
+      "Visita la biblioteca en el segundo piso.",
+      "No te pierdas la sala de conferencias en el tercer piso.",
+    ],
+  },
+  {
+    id: "Modulo E",
+    images: [require("./Modulos/Modulo_E/E_PRIMER_PISO.png")],
+    images: [require("./Modulos/Modulo_E/E_SEGUNDO_PISO.png")],
+    description:
+      "El Módulo E alberga una variedad de aulas y oficinas administrativas.",
+    recommendations: [
+      "Revisa las oficinas de administración en el primer piso.",
+      "Visita las aulas de cursos especiales en el segundo piso.",
+    ],
+  },
+  {
+    id: "Modulo F",
+    images: [require("./Modulos/Modulo_F/F.png")],
+    images: [require("./Modulos/Modulo_F/F_SEGUNDO_PISO.png")],
+    description:
+      "El Módulo F es conocido por sus amplios laboratorios de física y química.",
+    recommendations: [
+      "Visita los laboratorios de física en el segundo piso.",
+      "No te pierdas los experimentos de química en el tercer piso.",
+    ],
+  },
+  {
+    id: "Modulo G",
+    images: [require("./Modulos/Modulo_G/G.png")],
+    description:
+      "El Módulo G es un edificio moderno que incluye aulas y espacios de investigación.",
+    recommendations: [
+      "Visita el área de investigación en el primer piso.",
+      "Explora las aulas equipadas en el segundo piso.",
+    ],
+  },
+  {
+    id: "Modulo H",
+    images: [require("./Modulos/Modulo_H/H_PRIMER_PISO.png")],
+    images: [require("./Modulos/Modulo_H/H_SEGUNDO_PISO.png")],
+    description:
+      "El Módulo H destaca por su diseño espacioso y moderno, ideal para conferencias.",
+    recommendations: [
+      "Visita la sala de conferencias en el segundo piso.",
+      "No te pierdas el área de descanso en el primer piso.",
+    ],
+  },
+  {
+    id: "Modulo I",
+    images: [require("./Modulos/Modulo_I/I.png")],
+    description:
+      "El Módulo I está diseñado para albergar laboratorios y áreas de estudio grupal.",
+    recommendations: [
+      "Visita el laboratorio de informática en el segundo piso.",
+      "Aprovecha las áreas de estudio en el tercer piso.",
+    ],
+  },
+  {
+    id: "Modulo J",
+    images: [require("./Modulos/Modulo_J/J_PRIMER_PISO.png")],
+    images: [require("./Modulos/Modulo_J/J_SEGUNDO_PISO.png")],
+    description:
+      "El Módulo J es conocido por sus espacios de enseñanza interactivos.",
+    recommendations: [
+      "Visita las aulas interactivas en el primer piso.",
+      "Explora el laboratorio de robótica en el segundo piso.",
+    ],
+  },
+  {
+    id: "Modulo K",
+    images: [require("./Modulos/Modulo_K/K.png")],
+    description:
+      "El Módulo K cuenta con oficinas administrativas y áreas de soporte académico.",
+    recommendations: [
+      "Revisa las oficinas en el primer piso.",
+      "Explora las áreas de soporte técnico en el segundo piso.",
+    ],
+  },
+  {
+    id: "Modulo L",
+    images: [require("./Modulos/Modulo_L/L.png")],
+    description:
+      "El Módulo L ofrece una gran variedad de aulas y espacios de trabajo colaborativo.",
+    recommendations: [
+      "Visita las aulas en el primer piso.",
+      "Explora los espacios de trabajo colaborativo en el segundo piso.",
+    ],
+  },
+  {
+    id: "Modulo M",
+    images: [require("./Modulos/Modulo_M/M_PRIMER_PISO.png")],
+    images: [require("./Modulos/Modulo_M/M_SEGUNDO_PISO.png")],
+    description:
+      "El Módulo M es conocido por albergar laboratorios de alta tecnología.",
+    recommendations: [
+      "Visita el laboratorio de biotecnología en el primer piso.",
+      "No te pierdas los laboratorios de electrónica en el segundo piso.",
+    ],
+  },
+  {
+    id: "Modulo N",
+    images: [require("./Modulos/Modulo_N/N_PRIMER_PISO.png")],
+    images: [require("./Modulos/Modulo_N/N_SEGUNDO_PISO.png")],
+    description:
+      "El Módulo N cuenta con espacios de enseñanza moderna y oficinas administrativas.",
+    recommendations: [
+      "Visita las aulas en el primer piso.",
+      "Explora las oficinas de administración en el segundo piso.",
+    ],
+  },
+  {
+    id: "Modulo O",
+    images: [require("./Modulos/Modulo_O/O_PRIMER_PISO.png")],
+    images: [require("./Modulos/Modulo_O/O_SEGUNDO_PISO.png")],
+    description:
+      "El Módulo O alberga aulas especializadas para cursos de ingeniería y tecnología.",
+    recommendations: [
+      "Explora las aulas de ingeniería en el primer piso.",
+      "No te pierdas los laboratorios de tecnología en el segundo piso.",
+    ],
+  },
+  {
+    id: "Modulo P",
+    images: [require("./Modulos/Modulo_P/P.png")],
+    description:
+      "El Módulo P ofrece espacios de enseñanza interactivos y oficinas académicas.",
+    recommendations: [
+      "Visita las aulas interactivas en el primer piso.",
+      "Revisa las oficinas académicas en el segundo piso.",
+    ],
+  },
+  {
+    id: "Modulo Q",
+    images: [require("./Modulos/Modulo_Q/Q.png")],
+    description:
+      "El Módulo Q es un edificio especializado en investigación científica.",
+    recommendations: [
+      "Visita los laboratorios de investigación en el segundo piso.",
+      "Explora las áreas de estudio grupal en el primer piso.",
+    ],
+  },
+  {
+    id: "Modulo R",
+    images: [require("./Modulos/Modulo_R/R.png")],
+    description:
+      "El Módulo R es conocido por su enfoque en ciencias sociales y humanidades.",
+    recommendations: [
+      "Visita las aulas de ciencias sociales en el primer piso.",
+      "Explora la biblioteca de humanidades en el segundo piso.",
+    ],
+  },
+  {
+    id: "Modulo S",
+    images: [require("./Modulos/Modulo_S/S.png")],
+    description:
+      "El Módulo S alberga aulas y laboratorios de alta tecnología.",
+    recommendations: [
+      "Visita el laboratorio de informática en el segundo piso.",
+      "Explora las aulas interactivas en el primer piso.",
+    ],
+  },
+  {
+    id: "Modulo S2",
+    images: [require("./Modulos/Modulo_S/S2.png")],
+    description:
+      "El Módulo S alberga aulas y laboratorios de alta tecnología.",
+    recommendations: [
+      "Visita el laboratorio de informática en el segundo piso.",
+      "Explora las aulas interactivas en el primer piso.",
+    ],
+  },
+  {
+    id: "Modulo T",
+    images: [require("./Modulos/Modulo_T/T.png")],
+    description:
+      "El Módulo T se especializa en enseñanza técnica y profesional.",
+    recommendations: [
+      "Visita las aulas de tecnología en el primer piso.",
+      "Explora las áreas de práctica técnica en el segundo piso.",
+    ],
+  },
+  {
+    id: "Modulo U",
+    images: [require("./Modulos/Modulo_U/U.png")],
+    description:
+      "El Módulo U es conocido por su enfoque en la investigación avanzada.",
+    recommendations: [
+      "Visita los laboratorios de investigación avanzada en el segundo piso.",
+      "Explora las aulas especializadas en el primer piso.",
+    ],
+  },
+  {
+    id: "Modulo V",
+    images: [require("./Modulos/Modulo_V/V.png")],
+    description:
+      "El Módulo V cuenta con aulas y laboratorios especializados para cursos de tecnología.",
+    recommendations: [
+      "Visita las aulas tecnológicas en el primer piso.",
+      "No te pierdas los laboratorios en el segundo piso.",
+    ],
+  },
+  {
+    id: "Modulo V2",
+    images: [require("./Modulos/Modulo_V2/V2.png")],
+    description:
+      "El Módulo V2 es una extensión del Módulo V, con más espacio para investigación.",
+    recommendations: [
+      "Explora los nuevos laboratorios de investigación en el segundo piso.",
+      "Visita las salas de estudio en el tercer piso.",
+    ],
+  },
+  {
+    id: "Modulo W",
+    images: [
+      require("./Modulos/Modulo_W/W_PRIMER_PISO.png"),
+      require("./Modulos/Modulo_W/W_SEGUNDO_PISO.png"),
+      require("./Modulos/Modulo_W/W_TERCER_PISO.png"),
+    ],
+    description:
+      "El Módulo W es un edificio versátil que alberga múltiples aulas y áreas comunes.",
+    recommendations: [
+      "Explora las aulas en el primer y segundo piso.",
+      "Disfruta del área de estudio grupal en el tercer piso.",
+    ],
+  },
+  {
+    id: "Modulo X",
+    images: [require("./Modulos/Modulo_X/X.png")],
+    images: [require("./Modulos/Modulo_X/CONEXION_W-X.png")],
+    description:
+      "El Módulo X es un edificio moderno diseñado para albergar aulas y laboratorios de última generación.",
+    recommendations: [
+      "Visita el laboratorio de informática en el segundo piso.",
+      "No te pierdas la vista desde la terraza en el último piso.",
+    ],
+  },
+  {
+    id: "Modulo Y",
+    images: [require("./Modulos/Modulo_Y/Y_FRENTE.png")],
+    images: [require("./Modulos/Modulo_Y/Y_PRIMER_PISO.png")],
+    images: [require("./Modulos/Modulo_Y/Y_SEGUNDO_PISO.png")],
+    images: [require("./Modulos/Modulo_Y/Y_TERCER_PISO.png")],
+    description:
+      "El Módulo X es un edificio moderno diseñado para albergar aulas y laboratorios de última generación.",
     recommendations: [
       "Visita el laboratorio de informática en el segundo piso.",
       "No te pierdas la vista desde la terraza en el último piso.",
@@ -22,69 +287,33 @@ export const bottomSheetContents = [
   },
   {
     id: "Modulo Z",
-    images: [require("./assets/Modulos/Modulo_Z/Z.png")],
+    images: [require("./Modulos/Modulo_Z/Z.png")],
+    images: [require("./Modulos/Modulo_Z/Z_CHEDRAUI.png")],
     description:
       "El Módulo Z es conocido por su arquitectura innovadora y sus espacios versátiles para diferentes actividades académicas.",
-    rating: 4.2,
-    comments: [
-      {
-        user: "Yair",
-        text: "Me encanta la biblioteca de este módulo, siempre encuentro un lugar tranquilo para estudiar.",
-      },
-      {
-        user: "Hernandez",
-        text: "Los salones son espaciosos y bien iluminados.",
-      },
-    ],
     recommendations: [
       "Aprovecha las salas de estudio grupal en el tercer piso.",
       "Visita la cafetería en la planta baja, tienen excelente café.",
     ],
   },
   {
-    id: "Modulo W",
-    images: [
-      require("./assets/Modulos/Modulo_W/W PRIMER PISO.png"),
-      require("./assets/Modulos/Modulo_W/W SEGUNDO PISO.png"),
-      require("./assets/Modulos/Modulo_W/W TERCER PISO.png"),
-    ],
+    id: "Modulo Z1",
+    images: [require("./Modulos/Modulo_Z1/Z1.png")],
     description:
-      "El Módulo W es conocido por su arquitectura innovadora y sus espacios versátiles para diferentes actividades académicas.",
-    rating: 4.2,
-    comments: [
-      {
-        user: "Yair",
-        text: "Me encanta la biblioteca de este módulo, siempre encuentro un lugar tranquilo para estudiar.",
-      },
-      {
-        user: "Hernandez",
-        text: "Los salones son espaciosos y bien iluminados.",
-      },
-    ],
+      "El Módulo Z1 cuenta con aulas especializadas para cursos avanzados.",
     recommendations: [
-      "Aprovecha las salas de estudio grupal en el tercer piso.",
-      "Visita la cafetería en la planta baja, tienen excelente café.",
+      "Visita las aulas de cursos avanzados en el primer piso.",
+      "No te pierdas las áreas de descanso en el segundo piso.",
     ],
   },
   {
-    id: "Modulo V2",
-    images: [require("./assets/Modulos/Modulo_V2/V2.png")],
+    id: "Modulo Z2",
+    images: [require("./Modulos/Modulo_Z2/Z2.png")],
     description:
-      "El Módulo V2 es conocido por su arquitectura innovadora y sus espacios versátiles para diferentes actividades académicas.",
-    rating: 4.2,
-    comments: [
-      {
-        user: "Yair",
-        text: "Me encanta la biblioteca de este módulo, siempre encuentro un lugar tranquilo para estudiar.",
-      },
-      {
-        user: "Hernandez",
-        text: "Los salones son espaciosos y bien iluminados.",
-      },
-    ],
+      "El Módulo Z2 es una extensión del Módulo Z, diseñado para conferencias y eventos académicos.",
     recommendations: [
-      "Aprovecha las salas de estudio grupal en el tercer piso.",
-      "Visita la cafetería en la planta baja, tienen excelente café.",
+      "Visita el auditorio en el primer piso.",
+      "Explora las áreas de trabajo grupal en el segundo piso.",
     ],
   },
 ];
