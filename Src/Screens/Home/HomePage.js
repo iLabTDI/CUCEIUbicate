5a741cd96e553383f801bd8a53733d8efded9390
yairhdz24 <yairhdz107@gmail.com>
2024-09-27 17:21:14 -0600

    Manege errores al abrir la ruta suspender y ocultar animaciones, asi como comantarios en todo el codigo

--- Src/Screens/Home/HomePage.js
+++ Src/Screens/Home/HomePage.js
@@ -9,60 +9,74 @@ import {
   Animated,
   Alert,
 } from "react-native";
-import { useNavigation, useIsFocused } from "@react-navigation/native";
-import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
-import { faBars, faRoute, faUser } from "@fortawesome/free-solid-svg-icons";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
-import ImageZoom from "react-native-image-pan-zoom";
-import LottieView from "lottie-react-native";
-import { SearchRoute } from "./Components/SearchBarsComponent/SearchRoute";
-import { SpecificSearch } from "./Components/SearchBarsComponent/SearchSpecific";
-import { BottomSheetComponent } from "./Components/BottonSheetComponent/BottonSheet";
-import { MapWithPointsAndRoutes } from "./Components/MapComponent/MapPoints";
-import { points } from "./Components/MapComponent/data";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { getSession } from "../../auth/SessionManager";
-import { routesImages } from "./Routes/Route_data";
-import ChatbotButton from "../ChatBot/Chatboot_Button";
+import { useNavigation, useIsFocused } from "@react-navigation/native"; // Hooks de navegación y control de enfoque
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome"; // Iconos
+import { faBars, faRoute, faUser, faTimes } from "@fortawesome/free-solid-svg-icons"; // Iconos específicos
+import { GestureHandlerRootView } from "react-native-gesture-handler"; // Manejo de gestos
+import ImageZoom from "react-native-image-pan-zoom"; // Componente para hacer zoom en la imagen
+import LottieView from "lottie-react-native"; // Animaciones Lottie
+import { SearchRoute } from "./Components/SearchBarsComponent/SearchRoute"; // Componente de búsqueda de rutas
+import { SpecificSearch } from "./Components/SearchBarsComponent/SearchSpecific"; // Componente de búsqueda específica
+import { BottomSheetComponent } from "./Components/BottonSheetComponent/BottonSheet"; // Componente BottomSheet
+import { MapWithPointsAndRoutes } from "./Components/MapComponent/MapPoints"; // Mapa con puntos y rutas
+import { points } from "./Components/MapComponent/data"; // Datos de los puntos
+import AsyncStorage from "@react-native-async-storage/async-storage"; // Almacenamiento local asíncrono
+import { getSession } from "../../auth/SessionManager"; // Función para obtener la sesión
+import { routesImages } from "./Routes/Route_data"; // Datos de las rutas
+import ChatbotButton from "../ChatBot/Chatboot_Button"; // Componente del botón del chatbot
 
+// Obtener las dimensiones de la pantalla para cálculos responsivos
 const { width, height } = Dimensions.get("window");
 
 export const HomePage = () => {
+  // Hooks de navegación y control de si la pantalla está en foco
   const navigation = useNavigation();
+  const isFocused = useIsFocused();
+
+  // Referencias para componentes y animaciones
   const bottomSheetRef = useRef(null);
   const imageZoomRef = useRef(null);
-  const isFocused = useIsFocused();
-  const [selectedPoint, setSelectedPoint] = useState(null);
-  const [showSearchBar, setShowSearchBar] = useState(false);
-  const [selectedRouteImage, setSelectedRouteImage] = useState(null); // Imagen de la ruta
+
+  // Estados para manejar la lógica y la UI de la aplicación
+  const [selectedPoint, setSelectedPoint] = useState(null); // Punto seleccionado en el mapa
+  const [showSearchBar, setShowSearchBar] = useState(false); // Control de visibilidad de la barra de búsqueda
+  const [selectedRouteImage, setSelectedRouteImage] = useState(null); // Imagen de la ruta seleccionada
   const [currentMapImage, setCurrentMapImage] = useState(
-    require("./assets/images/mapa2.webp")
+    require("./assets/images/mapa.webp")
   ); // Imagen actual del mapa
-  const [showSpecificSearch, setShowSpecificSearch] = useState(false);
-  const [selectedIcon, setSelectedIcon] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [markedObject, setMarkedObject] = useState(null);
-  const [isBottomSheetVisible, setIsBottomSheetVisible] = useState(false);
+  const [showSpecificSearch, setShowSpecificSearch] = useState(false); // Control de visibilidad de la búsqueda específica
+  const [selectedIcon, setSelectedIcon] = useState(null); // Icono de perfil seleccionado
+  const [isLoading, setIsLoading] = useState(true); // Estado de carga
+  const [markedObject, setMarkedObject] = useState(null); // Objeto marcado en el mapa
+  const [isBottomSheetVisible, setIsBottomSheetVisible] = useState(false); // Control de visibilidad del BottomSheet
+  const [isRouteActive, setIsRouteActive] = useState(false); // Control de si una ruta está activa
+  const [activeRoutePoints, setActiveRoutePoints] = useState([]); // Puntos de la ruta activa
+
+  // Animación para el overlay del BottomSheet
   const fadeAnim = useRef(new Animated.Value(0)).current;
 
+  // Efecto que se ejecuta cuando la pantalla está enfocada
   useEffect(() => {
     if (isFocused) {
-      loadSelectedIcon();
-      checkSession();
+      loadSelectedIcon(); // Cargar el icono seleccionado
+      checkSession(); // Verificar la sesión del usuario
     }
   }, [isFocused]);
 
+  // Efecto para manejar la animación del overlay del BottomSheet
   useEffect(() => {
     Animated.timing(fadeAnim, {
-      toValue: isBottomSheetVisible ? 1 : 0,
+      toValue: isBottomSheetVisible ? 1 : 0, // Control de opacidad del overlay
       duration: 200,
       useNativeDriver: true,
     }).start();
   }, [isBottomSheetVisible, fadeAnim]);
 
+  // Función para verificar la sesión del usuario
   const checkSession = async () => {
-    const session = await getSession();
+    const session = await getSession(); // Obtener la sesión
     if (!session) {
+      // Si no hay sesión, redirigir al login
       navigation.reset({
         index: 0,
         routes: [{ name: "Login" }],
@@ -70,67 +84,91 @@ export const HomePage = () => {
     }
   };
 
+  // Función para cargar el icono de perfil seleccionado desde AsyncStorage
   const loadSelectedIcon = async () => {
     try {
-      const savedIcon = await AsyncStorage.getItem("selectedIcon");
+      const savedIcon = await AsyncStorage.getItem("selectedIcon"); // Obtener el icono almacenado
       if (savedIcon) {
-        setSelectedIcon(JSON.parse(savedIcon));
+        setSelectedIcon(JSON.parse(savedIcon)); // Establecer el icono seleccionado
       }
     } catch (error) {
-      console.error("Error loading selected icon:", error);
+      console.error("Error loading selected icon:", error); // Manejo de errores
     }
   };
 
+  // Función que se llama cuando se carga la imagen del mapa
   const handleImageLoad = () => {
-    setIsLoading(false);
+    setIsLoading(false); // Ocultar la pantalla de carga
   };
 
+  // Función que se ejecuta cuando se presiona un punto en el mapa
   const handlePointPress = (pointId) => {
-    setSelectedPoint(pointId);
-    setIsBottomSheetVisible(true);
-    bottomSheetRef.current?.expand();
+    setSelectedPoint(pointId); // Establecer el punto seleccionado
+    setIsBottomSheetVisible(true); // Mostrar el BottomSheet
+    bottomSheetRef.current?.expand(); // Expandir el BottomSheet
   };
 
+  // Función para alternar la visibilidad de la barra de búsqueda
   const toggleSearchBar = () => {
-    setShowSearchBar((prev) => !prev);
+    setShowSearchBar((prev) => !prev); // Cambiar el estado de la barra de búsqueda
   };
 
+  // Función para cerrar la barra de búsqueda
   const closeSearchBar = () => {
-    setShowSearchBar(false);
+    setShowSearchBar(false); // Ocultar la barra de búsqueda
   };
 
+  // Función para cerrar el BottomSheet
   const handleCloseBottomSheet = () => {
-    setIsBottomSheetVisible(false);
-    bottomSheetRef.current?.close();
+    setIsBottomSheetVisible(false); // Ocultar el BottomSheet
+    bottomSheetRef.current?.close(); // Cerrar el BottomSheet
   };
 
+  // Función para manejar la búsqueda específica
   const handleSpecificSearch = (pointId) => {
-    const selectedObject = points.find((point) => point.id === pointId);
+    const selectedObject = points.find((point) => point.id === pointId); // Encontrar el punto seleccionado
     if (selectedObject) {
-      setMarkedObject(selectedObject);
+      setMarkedObject(selectedObject); // Establecer el objeto marcado
     }
-    setShowSpecificSearch(false);
+    setShowSpecificSearch(false); // Ocultar la búsqueda específica
   };
 
+  // Función para manejar la búsqueda de rutas
   const handleSearch = ({ searchKey, reverseSearchKey }) => {
-    // Buscar primero la ruta directa, si no existe, buscar la ruta inversa
+    let routeImage;
+    let routePoints;
+
+    // Verificar si existe una imagen para la ruta seleccionada
     if (routesImages[searchKey]) {
-      setSelectedRouteImage(routesImages[searchKey]); // Guardar la imagen de la ruta
-      setCurrentMapImage(routesImages[searchKey]); // Reemplazar el mapa base con la ruta seleccionada
+      routeImage = routesImages[searchKey];
+      routePoints = searchKey.split('-'); // Dividir los puntos de la ruta
     } else if (routesImages[reverseSearchKey]) {
-      setSelectedRouteImage(routesImages[reverseSearchKey]); // Guardar la imagen de la ruta
-      setCurrentMapImage(routesImages[reverseSearchKey]); // Reemplazar el mapa base con la ruta inversa
+      routeImage = routesImages[reverseSearchKey];
+      routePoints = reverseSearchKey.split('-');
     }
-    setShowSearchBar(false);
+
+    // Si se encuentra la imagen de la ruta, establecer la ruta activa
+    if (routeImage) {
+      setSelectedRouteImage(routeImage); // Establecer la imagen de la ruta
+      setCurrentMapImage(routeImage); // Cambiar la imagen del mapa
+      setIsRouteActive(true); // Marcar la ruta como activa
+      setActiveRoutePoints(routePoints); // Establecer los puntos de la ruta activa
+    }
+
+    setShowSearchBar(false); // Ocultar la barra de búsqueda
   };
 
+  // Función para limpiar la ruta seleccionada
   const clearRoute = () => {
-    setSelectedRouteImage(null); // Limpiar la ruta seleccionada
-    setCurrentMapImage(require("./assets/images/mapa2.webp")); // Restaurar el mapa base
+    setSelectedRouteImage(null); // Limpiar la imagen de la ruta
+    setCurrentMapImage(require("./assets/images/mapa2.webp")); // Restablecer la imagen del mapa
+    setIsRouteActive(false); // Marcar la ruta como inactiva
+    setActiveRoutePoints([]); // Limpiar los puntos de la ruta activa
   };
 
   return (
     <View style={styles.container}>
+      {/* Pantalla de carga */}
       {isLoading && (
         <View style={styles.loadingContainer}>
           <LottieView
@@ -143,15 +181,19 @@ export const HomePage = () => {
         </View>
       )}
 
+      {/* Botón del menú */}
       <TouchableOpacity
         style={styles.menu_icon}
-        onPress={() => navigation.openDrawer()}>
+        onPress={() => navigation.openDrawer()}
+      >
         <FontAwesomeIcon icon={faBars} size={width * 0.06} color="white" />
       </TouchableOpacity>
 
+      {/* Botón de perfil */}
       <TouchableOpacity
         style={styles.profile_icon}
-        onPress={() => navigation.navigate("Perfil")}>
+        onPress={() => navigation.navigate("Perfil")}
+      >
         {selectedIcon ? (
           <Image source={selectedIcon} style={styles.profileImage} />
         ) : (
@@ -159,10 +201,12 @@ export const HomePage = () => {
         )}
       </TouchableOpacity>
 
+      {/* Botón de búsqueda */}
       <TouchableOpacity style={styles.search_icon} onPress={toggleSearchBar}>
         <FontAwesomeIcon icon={faRoute} size={width * 0.06} color="white" />
       </TouchableOpacity>
 
+      {/* Componente de búsqueda de rutas */}
       {showSearchBar && (
         <SearchRoute
           onClose={closeSearchBar}
@@ -171,6 +215,7 @@ export const HomePage = () => {
         />
       )}
 
+      {/* Componente de búsqueda específica */}
       <SpecificSearch
         points={points}
         onSearch={handleSpecificSearch}
@@ -178,6 +223,7 @@ export const HomePage = () => {
         setMarkedObject={setMarkedObject}
       />
 
+      {/* Contenedor principal del mapa */}
       <GestureHandlerRootView style={styles.imageContainer}>
         <ImageZoom
           ref={imageZoomRef}
@@ -192,10 +238,10 @@ export const HomePage = () => {
           maxScale={2}
           enableCenterFocus={false}
           useNativeDriver={true}
-          centerOn={{ x: 250, y: -20, scale: 0.9 }}>
-          {/* Muestra la imagen actual del mapa (base o con ruta seleccionada) */}
+          centerOn={{ x: 250, y: -20, scale: 0.9 }}
+        >
           <Image
-            source={currentMapImage} // Aqui se muestra la imagen actual del mapa
+            source={currentMapImage}
             style={styles.image}
             resizeMode="contain"
             onLoad={() => setTimeout(handleImageLoad, 3000)}
@@ -208,25 +254,32 @@ export const HomePage = () => {
             clearRoute={clearRoute}
             markedObject={markedObject}
             setMarkedObject={setMarkedObject}
+            isRouteActive={isRouteActive}
+            activeRoutePoints={activeRoutePoints}
           />
         </ImageZoom>
       </GestureHandlerRootView>
 
-      {selectedRouteImage && (
+      {/* Botón para finalizar la ruta */}
+      {isRouteActive && (
         <TouchableOpacity style={styles.finalizeButton} onPress={clearRoute}>
           <Text style={styles.finalizeButtonText}>Finalizar Ruta</Text>
         </TouchableOpacity>
       )}
 
+      {/* Overlay para el BottomSheet */}
       <Animated.View
         style={[styles.overlay, { opacity: fadeAnim }]}
         pointerEvents="none"
       />
-      <ChatbotButton />
 
+      {/* Botón del chatbot */}
+      {!isRouteActive && !showSearchBar && <ChatbotButton />}
+
+      {/* Componente BottomSheet */}
       <BottomSheetComponent
         ref={bottomSheetRef}
-        snapPoints={[ "50%", "75%"]}
+        snapPoints={["50%", "75%"]}
         selectedPoint={selectedPoint}
         isVisible={isBottomSheetVisible}
         onClose={handleCloseBottomSheet}
@@ -235,6 +288,7 @@ export const HomePage = () => {
   );
 };
 
+// Estilos del componente
 const styles = StyleSheet.create({
   container: { flex: 1 },
   loadingContainer: {
