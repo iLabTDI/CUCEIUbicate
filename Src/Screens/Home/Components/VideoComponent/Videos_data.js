73fe1b6f8b3def3a414aa6516541a92ca6c6c9f6
yairhdz24 <yairhdz107@gmail.com>
2024-10-04 01:28:29 -0600

    rytas de los datos de los videos

--- Src/Screens/Home/Components/VideoComponent/Videos_data.js
+++ Src/Screens/Home/Components/VideoComponent/Videos_data.js
@@ -1,7 +1,7 @@
 export const routeVideos = {
+  
+  /////// VIDEOS DE RUTAS MODULO X ////////
   "Modulo X - Modulo W": require("./route_videos/N-UCT1.mp4"),
-  // Agrega más rutas y sus correspondientes videos aquí
-  // Por ejemplo:
-  // "Modulo A - Modulo B": require("../assets/videos/A-B.mp4"),
-  // "Modulo C - Modulo D": require("../assets/videos/C-D.mp4"),
+  // mas rutas de videos correspondiendo la ruta
+
 };
