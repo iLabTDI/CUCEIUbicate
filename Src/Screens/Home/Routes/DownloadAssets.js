4932d68a27c34c1d186fb84c18852a7855c1f23e
yairhdz24 <yairhdz107@gmail.com>
2024-10-14 00:46:26 -0600

    modal de descarga con animacion

--- Src/Screens/Home/Routes/DownloadAssets.js
+++ Src/Screens/Home/Routes/DownloadAssets.js
@@ -1,217 +1,116 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, StyleSheet, TouchableOpacity, Modal } from "react-native";
-import * as FileSystem from "expo-file-system";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Modal,
+  Dimensions,
+  SafeAreaView,
+} from "react-native";
 import { supabase } from "../../../Api/lib/supabase";
+import * as FileSystem from "expo-file-system";
 import LottieView from "lottie-react-native";
-import { LinearGradient } from "expo-linear-gradient";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import {
   faCloudDownloadAlt,
-  faCheckCircle,
-  faExclamationTriangle,
-  faTimes,
+  faArrowRight,
 } from "@fortawesome/free-solid-svg-icons";
 
-export const DownloadAssets = ({ onClose }) => {
-  const [showModal, setShowModal] = useState(true);
-  const [downloadProgress, setDownloadProgress] = useState(0);
-  const [downloadStatus, setDownloadStatus] = useState("idle");
+export const DownloadAssets = ({ onClose, onViewDownload }) => {
+  const [showModal, setShowModal] = useState(false);
+  const [filesExist, setFilesExist] = useState(false);
 
   useEffect(() => {
-    if (showModal) {
-      checkExistingFiles();
-    }
-  }, [showModal]);
+    checkExistingFiles();
+  }, []);
 
   const checkExistingFiles = async () => {
     try {
-      const files = await FileSystem.readDirectoryAsync(
-        FileSystem.documentDirectory
-      );
-      const webpFiles = files.filter((file) => file.endsWith(".webp"));
-      if (webpFiles.length > 0) {
-        setDownloadStatus("completed");
-      }
-    } catch (error) {
-      console.error("Error checking existing files:", error);
-    }
-  };
-
-  const downloadImages = async () => {
-    setDownloadStatus("downloading");
-    try {
       const { data, error } = await supabase.storage
         .from("route_images")
-        .list("route_Images");
+        .list("nuevas");
 
       if (error) throw error;
 
-      const totalFiles = data.filter((file) =>
-        file.name.endsWith(".webp")
-      ).length;
-      let downloadedFiles = 0;
+      const fileInfoPromises = data
+        .filter((file) => file.name.endsWith(".webp"))
+        .map(async (file) => {
+          const localUri = `${FileSystem.documentDirectory}${file.name}`;
+          const fileExists = await FileSystem.getInfoAsync(localUri);
+          return fileExists.exists;
+        });
 
-      for (const file of data) {
-        if (file.name.endsWith(".webp")) {
-          const { data: imageData, error: downloadError } =
-            await supabase.storage
-              .from("route_images")
-              .download(`route_Images/${file.name}`);
+      const fileExistsResults = await Promise.all(fileInfoPromises);
+      const allFilesExist = fileExistsResults.every((exists) => exists);
 
-          if (downloadError) throw downloadError;
-
-          const reader = new FileReader();
-          reader.onload = async () => {
-            const base64String = reader.result.split(",")[1];
-            const localUri = `${FileSystem.documentDirectory}${file.name}`;
-
-            try {
-              await FileSystem.writeAsStringAsync(localUri, base64String, {
-                encoding: FileSystem.EncodingType.Base64,
-              });
-              console.log(`Imagen descargada y guardada en: ${localUri}`);
-              downloadedFiles++;
-              setDownloadProgress((downloadedFiles / totalFiles) * 100);
-            } catch (writeError) {
-              console.error(
-                "Error al guardar la imagen localmente:",
-                writeError
-              );
-              throw writeError;
-            }
-          };
-          reader.readAsDataURL(imageData);
-        }
+      setFilesExist(allFilesExist);
+      setShowModal(!allFilesExist);
+      if (allFilesExist) {
+        onDownloadComplete(); // Notifica que ya están descargados
       }
-
-      setDownloadStatus("completed");
-    } catch (err) {
-      console.error("Error al descargar imágenes:", err);
-      setDownloadStatus("error");
+    } catch (error) {
+      console.error("Error checking existing files:", error);
+      setShowModal(true);
     }
   };
 
-  const handleClose = () => {
-    setShowModal(false);
-    onClose();
-  };
-
-  const renderModalContent = () => {
-    switch (downloadStatus) {
-      case "idle":
-        return (
-          <>
-            <FontAwesomeIcon
-              icon={faCloudDownloadAlt}
-              size={50}
-              color="#0056b3"
-            />
-            <Text style={styles.modalTitle}>Descargar Archivos</Text>
-            <Text style={styles.modalText}>
-              Es necesario descargar los archivos para usar la aplicación sin
-              conexión.
-            </Text>
-            <TouchableOpacity
-              style={[styles.button, styles.primaryButton]}
-              onPress={downloadImages}>
-              <Text style={[styles.buttonText, styles.primaryButtonText]}>
-                Descargar
-              </Text>
-            </TouchableOpacity>
-          </>
-        );
-      case "downloading":
-        return (
-          <>
-            <LottieView
-              source={require("../../../assets/animations/Archivo.json")}
-              autoPlay
-              loop
-              style={styles.lottieAnimation}
-            />
-            <Text style={styles.modalTitle}>Descargando Archivos</Text>
-            <Text style={styles.modalText}>{`Progreso: ${Math.round(
-              downloadProgress
-            )}%`}</Text>
-            <View style={styles.progressBarContainer}>
-              <LinearGradient
-                colors={["#0056b3", "#007bff"]}
-                style={[styles.progressBar, { width: `${downloadProgress}%` }]}
-                start={{ x: 0, y: 0 }}
-                end={{ x: 1, y: 0 }}
-              />
-            </View>
-            <TouchableOpacity style={styles.closeButton} onPress={handleClose}>
-              <FontAwesomeIcon icon={faTimes} size={24} color="#333" />
-            </TouchableOpacity>
-          </>
-        );
-      case "completed":
-        return (
-          <>
-            <FontAwesomeIcon icon={faCheckCircle} size={50} color="#28a745" />
-            <Text style={styles.modalTitle}>Descarga Completada</Text>
-            <Text style={styles.modalText}>
-              Todos los archivos han sido descargados exitosamente.
-            </Text>
-            <TouchableOpacity
-              style={[styles.button, styles.primaryButton]}
-              onPress={handleClose}>
-              <Text style={[styles.buttonText, styles.primaryButtonText]}>
-                Cerrar
-              </Text>
-            </TouchableOpacity>
-          </>
-        );
-      case "error":
-        return (
-          <>
-            <FontAwesomeIcon
-              icon={faExclamationTriangle}
-              size={50}
-              color="#dc3545"
-            />
-            <Text style={styles.modalTitle}>Error en la Descarga</Text>
-            <Text style={styles.modalText}>
-              Ha ocurrido un error al descargar los archivos. Por favor,
-              inténtelo de nuevo más tarde.
-            </Text>
-            <TouchableOpacity
-              style={[styles.button, styles.primaryButton]}
-              onPress={handleClose}>
-              <Text style={[styles.buttonText, styles.primaryButtonText]}>
-                Cerrar
-              </Text>
-            </TouchableOpacity>
-          </>
-        );
-    }
-  };
-
+  if (filesExist) {
+    return null;
+  }
   return (
     <Modal
       animationType="fade"
       transparent={true}
       visible={showModal}
-      onRequestClose={handleClose}>
-      <View style={styles.modalOverlay}>
-        <View style={styles.modalContent}>{renderModalContent()}</View>
-      </View>
+      onRequestClose={onClose}>
+      <SafeAreaView style={styles.modalContainer}>
+        <View style={styles.modalContent}>
+          <LottieView
+            source={require("../../../assets/animations/Archivo.json")}
+            autoPlay
+            loop
+            style={styles.lottieAnimation}
+          />
+          <Text style={styles.title}>Archivos Necesarios</Text>
+          <Text style={styles.description}>
+            Para usar la aplicación sin conexión, es necesario descargar algunos
+            archivos.
+          </Text>
+          <TouchableOpacity
+            style={styles.downloadButton}
+            onPress={onViewDownload}>
+            <FontAwesomeIcon
+              icon={faCloudDownloadAlt}
+              size={24}
+              color="#FFFFFF"
+              style={styles.buttonIcon}
+            />
+            <Text style={styles.buttonText}>Ver Descarga</Text>
+            <FontAwesomeIcon
+              icon={faArrowRight}
+              size={24}
+              color="#FFFFFF"
+              style={styles.buttonIcon}
+            />
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
     </Modal>
   );
 };
 
 const styles = StyleSheet.create({
-  modalOverlay: {
+  modalContainer: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "rgba(0, 0, 0, 0.5)",
   },
   modalContent: {
-    backgroundColor: "white",
+    backgroundColor: "#FFFFFF",
     borderRadius: 20,
-    padding: 35,
+    padding: 32,
     alignItems: "center",
     shadowColor: "#000",
     shadowOffset: {
@@ -221,63 +120,52 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.25,
     shadowRadius: 4,
     elevation: 5,
-    width: "80%",
-  },
-  modalTitle: {
-    fontSize: 24,
-    fontWeight: "bold",
-    marginBottom: 15,
-    color: "#333",
-  },
-  modalText: {
-    fontSize: 16,
-    marginBottom: 20,
-    textAlign: "center",
-    color: "#666",
-  },
-  buttonContainer: {
-    flexDirection: "row",
-    justifyContent: "space-around",
-    width: "100%",
-  },
-  button: {
-    borderRadius: 20,
-    padding: 10,
-    elevation: 2,
-    backgroundColor: "#f8f9fa",
-    width: "40%",
-  },
-  buttonText: {
-    color: "#0056b3",
-    fontWeight: "bold",
-    textAlign: "center",
-  },
-  primaryButton: {
-    backgroundColor: "#0056b3",
-  },
-  primaryButtonText: {
-    color: "white",
+    width: "90%",
+    maxWidth: 400,
   },
   lottieAnimation: {
     width: 200,
     height: 200,
   },
-  progressBarContainer: {
-    width: "100%",
-    height: 10,
-    backgroundColor: "#e9ecef",
-    borderRadius: 5,
-    overflow: "hidden",
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#0033A0",
+    marginBottom: 16,
+    textAlign: "center",
   },
-  progressBar: {
-    height: "100%",
+  description: {
+    fontSize: 16,
+    color: "#666",
+    marginBottom: 24,
+    textAlign: "center",
+  },
+  downloadButton: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#0033A0",
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+    borderRadius: 30,
+    marginBottom: 16,
+  },
+  buttonText: {
+    color: "#FFFFFF",
+    fontSize: 18,
+    fontWeight: "bold",
+    marginHorizontal: 12,
+  },
+  buttonIcon: {
+    marginHorizontal: 8,
   },
   closeButton: {
-    position: "absolute",
-    right: 10,
-    top: 10,
-    padding: 10,
+    padding: 8,
+  },
+  closeButtonText: {
+    color: "#0033A0",
+    fontSize: 16,
+    fontWeight: "600",
   },
 });
 
-export default DownloadAssets;
