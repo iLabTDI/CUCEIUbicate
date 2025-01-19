877435015b8f3088207ec23c61562508dcfdf59c
yairhdz24 <yairhdz107@gmail.com>
2025-01-19 02:54:00 -0600

    Prueba #1 modulo onboardign quitado para apk

--- App.js
+++ App.js
@@ -6,49 +6,49 @@ import { LoginScreen } from "./Src/auth/LoginScreen";
 import { CompleteProfile } from "./Src/auth/CompleteProfile";
 import { RegisterScreen } from "./Src/auth/RegisterScreen";
 import { MyDrawer } from "./Src/Screens/Home/Components/SearchBarsComponent/MyDrawer";
-import { OnboardingScreen } from "./Src/auth/OnboardingScreen";
+// import { OnboardingScreen } from "./Src/auth/OnboardingScreen";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { LogBox } from 'react-native';
 
 const Stack = createNativeStackNavigator();
 const { width } = Dimensions.get('window');
-const MAX_WIDTH = 950; // Define el ancho máximo para limitar el tamaño de la aplicación
+  // const MAX_WIDTH = 950; // Define el ancho máximo para limitar el tamaño de la aplicación
 
-LogBox.ignoreLogs([
-  "[Reanimated] Reading from `value` during component render. Please ensure that you do not access the `value` property or use `get` method of a shared value while React is rendering a component.",
-  "Using Math.random is not cryptographically secure! Use bcrypt.setRandomFallback to set a PRNG."
-]);
+// LogBox.ignoreLogs([
+//   "[Reanimated] Reading from `value` during component render. Please ensure that you do not access the `value` property or use `get` method of a shared value while React is rendering a component.",
+//   "Using Math.random is not cryptographically secure! Use bcrypt.setRandomFallback to set a PRNG."
+// ]);
 
-const originalConsoleWarn = console.warn;
-console.warn = (message, ...args) => {
-  if (typeof message === 'string' && message.includes('[Reanimated]')) {
-    return;
-  }
-  originalConsoleWarn(message, ...args);
-};
+// const originalConsoleWarn = console.warn;
+// console.warn = (message, ...args) => {
+//   if (typeof message === 'string' && message.includes('[Reanimated]')) {
+//     return;
+//   }
+//   originalConsoleWarn(message, ...args);
+// };
 
 function App() {
-  const [isFirstLaunch, setIsFirstLaunch] = useState(null);
+  // const [isFirstLaunch, setIsFirstLaunch] = useState(null);
 
-  useEffect(() => {
-    const checkFirstLaunch = async () => {
-      try {
-        const value = await AsyncStorage.getItem("@onboarding_completeed");
-        setIsFirstLaunch(value == null);
-      } catch (error) {
-        console.error("Error al verificar el estado de onboarding:", error);
-      }
-    };
-    checkFirstLaunch();
-  }, []);
+  // useEffect(() => {
+  //   const checkFirstLaunch = async () => {
+  //     try {
+  //       const value = await AsyncStorage.getItem("@onboarding_completeed");
+  //       setIsFirstLaunch(value == null);
+  //     } catch (error) {
+  //       console.error("Error al verificar el estado de onboarding:", error);
+  //     }
+  //   };
+  //   checkFirstLaunch();
+  // }, []);
 
-  if (isFirstLaunch === null) {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <ActivityIndicator size={24} color="#000000" />
-      </View>
-    );
-  }
+  // if (isFirstLaunch === null) {
+  //   return (
+  //     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+  //       <ActivityIndicator size={24} color="#000000" />
+  //     </View>
+  //   );
+  // }
 
   return (
     <NavigationContainer>
@@ -56,15 +56,15 @@ function App() {
         backgroundColor="#000000"
         barStyle={Platform.OS === "ios" ? "dark-content" : "light-content"}
       />
-      <View style={[styles.container, { width: width > MAX_WIDTH ? MAX_WIDTH : '100%', alignSelf: 'center' }]}>
+      <View style={[styles.container, { width: width, alignSelf: 'center' }]}>
         <Stack.Navigator>
-          {isFirstLaunch && (
+          {/* {isFirstLaunch && (
             <Stack.Screen
               name="Onboarding"
               component={OnboardingScreen}
               options={{ headerShown: false }}
             />
-          )}
+          )} */}
           <Stack.Screen
             name="Login"
             component={LoginScreen}
