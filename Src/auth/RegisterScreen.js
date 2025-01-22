929ec9cb95a930bd8cb03dd48af2fb003a47ff42
yairhdz24 <yairhdz107@gmail.com>
2025-01-22 01:32:19 -0600

    Remove api componete and refactorization of regex in the password register

--- Src/auth/RegisterScreen.js
+++ Src/auth/RegisterScreen.js
@@ -48,11 +48,9 @@ export const RegisterScreen = () => {
   const allowedDomains = [
     "alumnos.udg.mx",
     "gmail.com",
-    "hotmail.com",
-    "outlook.com",
   ];
   const emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
-  const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+  const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[^A-Za-z0-9]).{8,}$/;
 
   const navigation = useNavigation();
 
