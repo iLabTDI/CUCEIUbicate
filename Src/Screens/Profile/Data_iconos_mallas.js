6340e90e4dd409bd186f83d8829031166c8ef61e
yairhdz24 <yairhdz107@gmail.com>
2025-02-13 19:12:33 -0600

    Remove unused image files and components related to map points

--- Src/Screens/Profile/Data_iconos_mallas.js
+++ Src/Screens/Profile/Data_iconos_mallas.js
@@ -13,10 +13,10 @@ export const animalIcons = [
   // { id: "11", uri: require("./Iconos_animales/oveja.png") },
   // { id: "12", uri: require("./Iconos_animales/perro.png") },
   { id: "13", uri: require("./Iconos_animales/tigre.png") },
-  { id: "14", uri: require("./Iconos_animales/tortuga.png") },
+  { id: "14", uri: require("./Iconos_animales/tortugaa.png") },
   { id: "15", uri: require("./Iconos_animales/vaca.png") },
   { id: "16", uri: require("./Iconos_animales/anaconda.png") },
-  { id: "1", uri: require("./Iconos_animales/abeja.png") },
+  { id: "1", uri: require("./Iconos_animales/aabeja.png") },
   { id: "6", uri: require("./Iconos_animales/coolcat.png") },
   { id: "10", uri: require("./Iconos_animales/mono.png") },
   { id: "12", uri: require("./Iconos_animales/perro.png") },
@@ -25,13 +25,13 @@ export const animalIcons = [
 //     { id: "2", uri: require("./Iconos_animales/ajolote.png") },
 //     { id: "3", uri: require("./Iconos_animales/aguila.png") },
 //     { id: "4", uri: require("./Iconos_animales/cangrejo.png") },
-//     { id: "5", uri: require("./Iconos_animales/conejo.png") },
+//     { id: "5", uri: require("./Iconos_animales/conejoo.png") },
 //     { id: "7", uri: require("./Iconos_animales/cucaracha.png") },
 //     { id: "8", uri: require("./Iconos_animales/elefante.png") },
 //     { id: "9", uri: require("./Iconos_animales/leon.png") },
 //     { id: "11", uri: require("./Iconos_animales/oveja.png") },
 //     { id: "13", uri: require("./Iconos_animales/tigre.png") },
-//     { id: "14", uri: require("./Iconos_animales/tortuga.png") },
+//     { id: "14", uri: require("./Iconos_animales/tortugaa.png") },
 //     { id: "15", uri: require("./Iconos_animales/vaca.png") },
 //     { id: "16", uri: require("./Iconos_animales/anaconda.png") },
 //   ];
@@ -56,10 +56,4 @@ export const careerImages = {
   LQUI: require("./Mallas_Curriculares/malla_LQUI.png"),
 };
 
-// export const careerPdfs = {
-//   INCO: require("./Mallas_Curriculares/INCO.pdf"),
-//   INFO: require("./Mallas_Curriculares/INFO.pdf"),
-//   LOGT: require("./Mallas_Curriculares/LOGT.pdf"),
-//   // etc.
-// };
 
